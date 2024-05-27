ff264ae1daef49d4d7cf5d0bac70d6c8d68d778a
Pchelkin-Aleksey <xxeecc2001@gmail.com>
2024-05-27 22:42:59 +0700

    lesson 3. 1

--- src/main/java/com/example/coffee_time/controller/ProductController.java
+++ src/main/java/com/example/coffee_time/controller/ProductController.java
@@ -1,17 +1,35 @@
 package com.example.coffee_time.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/GET")
+@RequestMapping("/product")
 public class ProductController {
+    @GetMapping("/")
+    public String getProduct(){
+        return "templates/product/list.html";
+    }
 
-    @GetMapping("/product")
-    public void getProduct(){
+    @GetMapping("/new")
+    public String getProductNew(){
+        return "templates/product/form.html";
+    }
+
+    @GetMapping("/{id}")
+    public void getNewProduct(){
         return ;
     }
 
+    @PostMapping("/save")
+    public void save(){
+    }
+
+    @GetMapping("/{id}/delete")
+    public void delete(){
+        return ;
+    }
 
 }
