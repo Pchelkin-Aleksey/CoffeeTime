a79ce26f6f39cd28d3464026a46012cfdfe9fa0e
Alex Pchelkin <xxeecc2001@gmail.com>
2024-05-29 14:40:14 +0700

    making changes from a web form

--- src/main/java/com/example/coffee_time/controller/ProductController.java
+++ src/main/java/com/example/coffee_time/controller/ProductController.java
@@ -1,13 +1,25 @@
 package com.example.coffee_time.controller;
 
+import com.example.coffee_time.dto.ProductDto;
+import com.example.coffee_time.entity.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
     @GetMapping("/product")
     public String getProduct(){
         return "/product/form";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(@RequestParam String name,@RequestParam String description){
+        ProductDto productDto = new ProductDto( name, description);
+
+        return "/product/list";
     }
 
 }
--- src/main/java/com/example/coffee_time/dto/ProductDto.java
+++ src/main/java/com/example/coffee_time/dto/ProductDto.java
@@ -1,11 +1,18 @@
 package com.example.coffee_time.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductDto {
     private Long id;
     private String name;
     private String description;
+
+    public ProductDto(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
 
 }
