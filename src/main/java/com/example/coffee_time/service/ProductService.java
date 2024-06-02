31b101d7c1ee93810c738b2f3af7dd9000d69b10
Pchelkin-Aleksey <xxeecc2001@gmail.com>
2024-06-02 19:02:45 +0700

    add lesson 3

--- src/main/java/com/example/coffee_time/service/ProductService.java
+++ src/main/java/com/example/coffee_time/service/ProductService.java
@@ -5,6 +5,7 @@ import com.example.coffee_time.entity.Product;
 import com.example.coffee_time.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.example.coffee_time.utils.MappingUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,5 +13,26 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final MappingUtils mappingUtils;
+    public List<ProductDto> findAll() {
+        return productRepository.findAll().stream()
+                .map(mappingUtils::mapToProductDto)
+                .collect(Collectors.toList());
+    }
+    public ProductDto findById(Long id) {
+        return  mappingUtils.mapToProductDto(productRepository.findById(id).orElse(new Product()));
+    }
+
+    public List<ProductDto> findAllProducts() {
+        return mappingUtils.productEntitytoDTOList(productRepository.findAll());
+    }
+    public ProductDto saveProduct(ProductDto product) {
+        return mappingUtils.mapToProductDto(productRepository.save(mappingUtils.mapToProductEntity(product)));
+    }
+    public void delete(ProductDto product) {
+        productRepository.delete(mappingUtils.mapToProductEntity(product));
+    }
 
 }
